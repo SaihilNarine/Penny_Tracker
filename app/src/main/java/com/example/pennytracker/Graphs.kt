fd5fb0966e9b46f37b64ef47bc300ef1eb636319
LeeyahR <ST10440018@vcconnect.edu.za>
2026-06-01 18:15:19 +0200

    Filtering added to graphs

--- app/src/main/java/com/example/pennytracker/Graphs.kt
+++ app/src/main/java/com/example/pennytracker/Graphs.kt
@@ -1,7 +1,12 @@
 package com.example.pennytracker
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.graphics.Color
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import data.ChartData
 import com.github.mikephil.charting.charts.BarChart
@@ -15,6 +20,12 @@ class Graphs : AppCompatActivity() {
     private lateinit var pieChart: PieChart
     private lateinit var barChart: BarChart
 
+    //Filter
+    private lateinit var edtFilterDate : EditText
+    private lateinit var btnFilter : Button
+
+    private var selectedDate = ""
+
     private val dbRef =
         FirebaseDatabase.getInstance()
             .getReference("chartData")
@@ -27,13 +38,26 @@ class Graphs : AppCompatActivity() {
         barChart = findViewById(R.id.barChart)
         pieChart = findViewById(R.id.pieChart)
 
+        //Filter typecasting
+        edtFilterDate = findViewById(R.id.edtFilterDate)
+        btnFilter = findViewById(R.id.btnFilter)
+
         pieChart.setNoDataText("Loading data...")
         barChart.setNoDataText("Loading data...")
 
         loadData()
+
+        //Date picker
+        edtFilterDate.setOnClickListener {
+            showDatePicker()
+        }
+
+        btnFilter.setOnClickListener {
+            loadData(selectedDate)
+        }
     }
 
-    private fun loadData() {
+    private fun loadData(filterDate: String = "") {
 
         dbRef.addValueEventListener(
             object : ValueEventListener {
@@ -41,15 +65,11 @@ class Graphs : AppCompatActivity() {
                 override fun onDataChange(
                     snapshot: DataSnapshot
                 ) {
+                    val pieEntries = ArrayList<PieEntry>()
 
-                    val pieEntries =
-                        ArrayList<PieEntry>()
+                    val barEntries = ArrayList<BarEntry>()
 
-                    val barEntries =
-                        ArrayList<BarEntry>()
-
-                    val labels =
-                        ArrayList<String>()
+                    val labels = ArrayList<String>()
 
                     var index = 0f
 
@@ -62,25 +82,27 @@ class Graphs : AppCompatActivity() {
 
                         if (item != null) {
 
-                            pieEntries.add(
-                                PieEntry(
-                                    item.value,
-                                    item.category
+                            if (filterDate.isEmpty() || item.date == filterDate) {
+
+
+                                pieEntries.add(
+                                    PieEntry(
+                                        item.value,
+                                        item.category
+                                    )
                                 )
-                            )
 
-                            barEntries.add(
-                                BarEntry(
-                                    index,
-                                    item.value
+                                barEntries.add(
+                                    BarEntry(
+                                        index,
+                                        item.value
+                                    )
                                 )
-                            )
 
-                            labels.add(
-                                item.category
-                            )
+                                labels.add(item.category)
 
-                            index++
+                                index++
+                            }
                         }
                     }
 
@@ -147,4 +169,29 @@ class Graphs : AppCompatActivity() {
         barChart.animateY(1000)
         barChart.invalidate()
     }
+
+    @SuppressLint("DefaultLocale")
+    private fun showDatePicker(){
+        val calendar = Calendar.getInstance()
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            {_, year, month, day -> selectedDate =
+                String.format(
+                    "%02d/%02d/%04d",
+                    day,
+                    month + 1,
+                    year
+                )
+
+                edtFilterDate.setText(selectedDate)
+            },
+
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.show()
+    }
+
 }
